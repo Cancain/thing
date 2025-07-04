aa1382445cc2c789774e8719f2c6e0f55a7a5627
Tomas Eriksson <eriksson.tomas@gmail.com>
2025-07-04 15:02:36 +0200

    inputhandler and restructure

---
+++ src/inputHandler.cpp
@@ -0,0 +1,172 @@
+#include "inputHandler.h"
+#include <iostream>
+
+InputHandler::InputHandler()
+{
+}
+
+InputHandler::~InputHandler()
+{
+}
+
+void InputHandler::handleEvents(SDL_Event &event)
+{
+    switch (event.type)
+    {
+    case SDL_QUIT:
+        handleQuit();
+        break;
+    case SDL_KEYDOWN:
+        handleKeyDown(event.key.keysym.sym);
+        break;
+    case SDL_KEYUP:
+        handleKeyUp(event.key.keysym.sym);
+        break;
+    case SDL_MOUSEBUTTONDOWN:
+        handleMouseDown(event.button.button, event.button.x, event.button.y);
+        break;
+    case SDL_MOUSEBUTTONUP:
+        handleMouseUp(event.button.button, event.button.x, event.button.y);
+        break;
+    }
+}
+
+void InputHandler::pollEvents()
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        handleEvents(event);
+    }
+}
+
+void InputHandler::onKeyDown(SDL_Keycode key, KeyCallback callback)
+{
+    m_keyDownCallbacks[key].push_back(callback);
+}
+
+void InputHandler::onKeyUp(SDL_Keycode key, KeyCallback callback)
+{
+    m_keyUpCallbacks[key].push_back(callback);
+}
+
+void InputHandler::onEscapeDown(KeyCallback callback)
+{
+    onKeyDown(SDLK_ESCAPE, callback);
+}
+
+void InputHandler::onSpaceDown(KeyCallback callback)
+{
+    onKeyDown(SDLK_SPACE, callback);
+}
+
+void InputHandler::onMouseClick(int button, MouseCallback callback)
+{
+    m_mouseCallbacks[button].push_back(callback);
+}
+
+void InputHandler::onLeftClick(MouseCallback callback)
+{
+    onMouseClick(SDL_BUTTON_LEFT, callback);
+}
+
+void InputHandler::onRightClick(MouseCallback callback)
+{
+    onMouseClick(SDL_BUTTON_RIGHT, callback);
+}
+
+void InputHandler::onQuit(KeyCallback callback)
+{
+    m_quitCallbacks.push_back(callback);
+}
+
+void InputHandler::removeKeyCallback(SDL_Keycode key, bool isKeyDown)
+{
+    if (isKeyDown)
+    {
+        m_keyDownCallbacks.erase(key);
+    }
+    else
+    {
+        m_keyUpCallbacks.erase(key);
+    }
+}
+
+void InputHandler::removeMouseCallback(int button)
+{
+    m_mouseCallbacks.erase(button);
+}
+
+void InputHandler::removeQuitCallback()
+{
+    m_quitCallbacks.clear();
+}
+
+bool InputHandler::isKeyPressed(SDL_Keycode key) const
+{
+    auto it = m_keyStates.find(key);
+    return (it != m_keyStates.end()) ? it->second : false;
+}
+
+bool InputHandler::isMouseButtonPressed(int button) const
+{
+    auto it = m_mouseButtonStates.find(button);
+    return (it != m_mouseButtonStates.end()) ? it->second : false;
+}
+
+void InputHandler::handleKeyDown(SDL_Keycode key)
+{
+    m_keyStates[key] = true;
+
+    auto it = m_keyDownCallbacks.find(key);
+    if (it != m_keyDownCallbacks.end())
+    {
+        for (auto &callback : it->second)
+        {
+            callback();
+        }
+    }
+}
+
+void InputHandler::handleKeyUp(SDL_Keycode key)
+{
+    m_keyStates[key] = false;
+
+    auto it = m_keyUpCallbacks.find(key);
+    if (it != m_keyUpCallbacks.end())
+    {
+        for (auto &callback : it->second)
+        {
+            callback();
+        }
+    }
+}
+
+void InputHandler::handleMouseDown(int button, int x, int y)
+{
+    m_mouseButtonStates[button] = true;
+
+    auto it = m_mouseCallbacks.find(button);
+    if (it != m_mouseCallbacks.end())
+    {
+        for (auto &callback : it->second)
+        {
+            callback(x, y);
+        }
+    }
+}
+
+void InputHandler::handleMouseUp(int button, int x, int y)
+{
+    (void)x; // Suppress unused parameter warning
+    (void)y; // Suppress unused parameter warning
+    m_mouseButtonStates[button] = false;
+}
+
+void InputHandler::handleQuit()
+{
+    for (auto &callback : m_quitCallbacks)
+    {
+        callback();
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,85 @@
 #include <SDL2/SDL.h>
 #include "window.h"
 #include "dot.h"
+#include "inputHandler.h"
 
-const int WINDOW_WIDTH = 640;
-const int WINDOW_HEIGHT = 480;
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+const int TARGET_FPS = 60;
+const int FRAME_DELAY = 1000 / TARGET_FPS;
 
 int main()
 {
-    Window window(WINDOW_WIDTH, WINDOW_HEIGHT, "SDL2 Test");
-    window.init();
-    window.create();
-    window.renderBackground();
-    window.present();
-    Dot dot(100, 100, 10, {255, 0, 0, 255});
-    dot.draw(window.getRenderer());
-    window.present();
-    system("clear");
-    std::cout << "SDL initialized successfully!" << std::endl;
-    std::cout << "Press Enter to continue...";
-    std::cin.get();
-    SDL_Quit();
+    Window window(WINDOW_WIDTH, WINDOW_HEIGHT, "SDL2 Game");
+
+    if (window.init() != 0)
+    {
+        std::cout << "Failed to initialize SDL!" << std::endl;
+        return 1;
+    }
+
+    if (window.create() != 0)
+    {
+        std::cout << "Failed to create window!" << std::endl;
+        return 1;
+    }
+
+    // Create a test dot
+    SDL_Color dotColor = {255, 0, 0, 255}; // Red
+    Dot dot(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 20, dotColor);
+
+    bool running = true;
+
+    // Create input handler
+    InputHandler inputHandler;
+
+    // Set up callbacks
+    inputHandler.onEscapeDown([&running]()
+                              {
+                                std::cout << "Escape key pressed" << std::endl;
+                                running = false; });
+
+    inputHandler.onSpaceDown([&dot]()
+                             {
+                                std::cout << "Space key pressed" << std::endl;
+                                dot.setColor({0, 255, 0, 255}); });
+
+    inputHandler.onLeftClick([&dot](int x, int y)
+                             {
+                                std::cout << "Left click at " << x << ", " << y << std::endl;
+                                dot.setPosition(x, y); });
+
+    inputHandler.onQuit([&running]()
+                        {
+                            std::cout << "Quit event received" << std::endl;
+                            running = false; });
+    Uint32 frameStart;
+    int frameTime;
+
+    // Game loop
+    while (running)
+    {
+        frameStart = SDL_GetTicks();
+
+        // Handle events
+        inputHandler.pollEvents();
+
+        // Update game state
+        dot.update();
+
+        // Render
+        window.renderBackground(0, 0, 0); // Black background
+        dot.draw(window.getRenderer());
+        window.present();
+
+        // Frame rate limiting
+        frameTime = SDL_GetTicks() - frameStart;
+        if (frameTime < FRAME_DELAY)
+        {
+            SDL_Delay(FRAME_DELAY - frameTime);
+        }
+    }
+
+    std::cout << "Game ended successfully!" << std::endl;
     return 0;
 }
